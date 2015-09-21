a4e2f9777e3b7aabd1fa9473391a495be43a58c8
Greg Heffernan (desktop) <gregheff@gmail.com>
2015-09-21 14:39:05 -0400

    Integrating the text !

---
+++ Assets/Custom Scripts/TextReader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextRead : MonoBehaviour
+{
+
+    public TextAsset textFile;
+    string[] dialogLines;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Make sure there this a text
+        // file assigned before continuing
+        if (textFile != null)
+        {
+            // Add each line of the text file to
+            // the array using the new line
+            // as the delimiter
+            dialogLines = (textFile.text.Split('\n'));
+        }
+    }
+}
