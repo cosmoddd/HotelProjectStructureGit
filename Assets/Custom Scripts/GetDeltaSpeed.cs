0f39e90a529cfc65f4d9bd3c3e7513e85ea52910
Greg Heffernan <gregheff@gmail.com>
2015-10-08 18:02:43 -0400

    opening and closing a drawer.  interesting....

---
+++ Assets/Custom Scripts/GetDeltaSpeed.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class   GetDeltaSpeed : MonoBehaviour
+{
+    // store how much the mouse has moved since the last frame
+    public Vector3 mouseDelta = Vector3.zero;
+
+    private Vector3 lastMousePosition = Vector3.zero;
+
+
+
+
+    void Update()
+    {
+        mouseDelta = Input.mousePosition - lastMousePosition;
+
+        lastMousePosition = Input.mousePosition;
+
+        //Debug.Log(mouseDelta.ToString("F3"));
+    }
+}
---
+++ Assets/SmoothMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothMove : MonoBehaviour {
+
+    public float target;
+    public float currentValue;
+    public float time;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        updateTarget();
+	
+	}
+
+    void updateTarget()
+    {
+        currentValue = Mathf.Lerp(currentValue, target, Time.deltaTime * Statics.gameTime); //
+
+    }
+}
