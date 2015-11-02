a70aa6dac123f72c32b20083ec2708e3146a2acc
Greg Heffernan (desktop) <gregheff@gmail.com>
2015-11-02 12:43:26 -0500

    Fun with mouse cursor control

---
+++ Assets/Cos_Scripts/MouseLock.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLock : MonoBehaviour {
+
+    public bool CursorLockedVar;
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    //    Cursor.visible = (true);
+        CursorLockedVar = (true);
+
+    }
+    void Update()
+
+    {
+        if (Input.GetKeyDown("escape") && !CursorLockedVar)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+         //   Cursor.visible = (false);
+            CursorLockedVar = (true);
+        }
+        else if (Input.GetKeyDown("escape") && CursorLockedVar)
+        {
+            Cursor.lockState = CursorLockMode.None;
+         //   Cursor.visible = (true);
+            CursorLockedVar = (false);
+        }
+    }
+    
+}
