01f48464cf49b53aa2044c5fa23134d4b344ef19
Greg Heffernan (desktop) <gregheff@gmail.com>
2015-11-04 17:19:18 -0500

    cleaning house a little bit

--- Assets/GetAxiss.cs
+++ Assets/GetAxiss.cs
@@ -5,11 +5,13 @@ public class GetAxiss : MonoBehaviour
 {
     public float horizontalSpeed = 2.0F;
     public float verticalSpeed = 2.0F;
+    public float h;
+    public float v;
 
     void Update()
     {
-        float h = horizontalSpeed * Input.GetAxis("Mouse X");
-        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        h = horizontalSpeed * Input.GetAxis("Mouse X");
+        v = verticalSpeed * Input.GetAxis("Mouse Y");
         transform.Rotate(v, h, 0);
     }
 }
