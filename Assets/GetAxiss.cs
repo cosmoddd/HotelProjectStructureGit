485290b4a6cef8931cd70d4655eb7ecc2247506b
Greg Heffernan <gregheff@gmail.com>
2015-11-04 12:14:42 -0500

    so close on the doors

---
+++ Assets/GetAxiss.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetAxiss : MonoBehaviour
+{
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(v, h, 0);
+    }
+}
