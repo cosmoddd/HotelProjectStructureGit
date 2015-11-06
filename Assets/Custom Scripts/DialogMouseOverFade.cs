62e38cce9b5856c7bbf1f88e2053fe898f349120
Greg Heffernan <gregheff@gmail.com>
2015-11-05 19:14:58 -0500

    the latest madness

---
+++ Assets/Custom Scripts/DialogMouseOverFade.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogMouseOverFade : MonoBehaviour
+{
+
+    //public GameObject objectToFade;  // object selection for fading (eh?)
+    public Color objectColor;
+    public float currentValue;
+    public float target;
+    //public Material mat;  // material selection for fading
+    public TypogenicText text;
+    Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        target = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        objectColor.a = currentValue;  // the key for lerping colors
+                                       //  mat.color = objectColor;  // assigning the color to the variable in question
+        text.ColorTopLeft.a = currentValue;
+
+        updateAlpha();
+
+    }
+
+    void OnMouseEnter()
+    {
+
+        Debug.Log("step in the areana");
+        target = 1;
+    }
+
+    void OnMouseExit()
+    {
+
+        Debug.Log("leave the areana");
+        target = 0;
+    }
+
+
+
+    void updateAlpha()
+    {
+        currentValue = Mathf.Lerp(currentValue, target, Time.deltaTime * Statics.gameTime);
+
+    }
+
+}
