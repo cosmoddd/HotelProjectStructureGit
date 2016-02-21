e4efef1869f40c89d55008eb0c0a91a253153658
Greg Heffernan (desktop) <gregheff@gmail.com>
2016-02-21 11:32:43 -0500

    scene manager works, now to update unity

---
+++ Assets/Custom Scripts/LevelLoadScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelLoadScript : MonoBehaviour {
+
+	public string sceneName;
+	// Use this for initialization
+	/*
+	void Start () {
+	
+	}
+*/	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			SceneManager.UnloadScene(sceneName);
+		}
+
+	}
+
+
+	void OnTriggerEnter()
+	{
+		LoadThisDamnScene(sceneName);
+	}
+
+	void OnTriggerExit()
+	{
+		UnloadThisDamnScene(sceneName);
+	}
+
+	void LoadThisDamnScene(string l){
+
+		SceneManager.LoadScene(l, LoadSceneMode.Additive);
+		sceneName = l;
+
+	}
+
+	void UnloadThisDamnScene(string l){
+
+		SceneManager.UnloadScene(l);
+	}
+
+//	void SceneLoadCheck
+
+	//SceneManager.LoadSceneMode
+
+
+}
