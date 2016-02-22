3f140bc7984a46b8a3fd4c2e9ce88e03fcc623a6
Greg Heffernan (desktop) <gregheff@gmail.com>
2016-02-22 01:12:45 -0500

    strong position going into final

---
+++ Assets/Custom Scripts/KeepThisObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepThisObject : MonoBehaviour
+{
+	
+public GameObject objectToKeep;
+
+	void Awake() {
+
+		objectToKeep = this.gameObject;
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
