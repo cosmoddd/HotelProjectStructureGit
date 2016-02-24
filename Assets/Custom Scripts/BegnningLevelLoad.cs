c61a2a2dcd736c53ae68a55469c8bd68fab3fc5a
Greg Heffernan (desktop) <gregheff@gmail.com>
2016-02-24 13:07:05 -0500

    shader culling

--- Assets/Custom Scripts/BegnningLevelLoad.cs
+++ Assets/Custom Scripts/BegnningLevelLoad.cs
@@ -7,7 +7,7 @@ public class BegnningLevelLoad : MonoBehaviour {
 //This script loads all the scenes at the beginning of the game that need to be loaded.
 	public string[] sceneName;
 
-	void Awake(){
+	void Start(){
 
 		for (int i = 0; i < sceneName.Length; i++)
 		{
