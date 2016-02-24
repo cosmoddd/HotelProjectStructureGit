33961d04a72d1d29d4b3a57f0c7a53f61b8dfd37
Greg Heffernan (desktop) <gregheff@gmail.com>
2016-02-24 12:02:53 -0500

    finessing the level loading situation

---
+++ Assets/Custom Scripts/BegnningLevelLoad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BegnningLevelLoad : MonoBehaviour {
+
+//This script loads all the scenes at the beginning of the game that need to be loaded.
+	public string[] sceneName;
+
+	void Awake(){
+
+		for (int i = 0; i < sceneName.Length; i++)
+		{
+			if (SceneManager.GetSceneByName(sceneName[i]).isLoaded == false)
+			{
+			SceneManager.LoadScene(sceneName[i], LoadSceneMode.Additive);
+			}
+		}
+		}
+}
