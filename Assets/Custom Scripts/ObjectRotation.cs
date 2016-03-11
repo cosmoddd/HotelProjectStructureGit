97a33bc976e449c8f35b7101015681aab3f384dd
Greg Heffernan (desktop) <gregheff@gmail.com>
2016-03-11 13:03:15 -0500

    object rotation breakthrough - using separate camera layer --- now roads?

--- Assets/Custom Scripts/ObjectRotation.cs
+++ Assets/Custom Scripts/ObjectRotation.cs
@@ -4,15 +4,15 @@ using System.Collections;
 public class ObjectRotation : MonoBehaviour {
 
 	public float speed = 1;
-	public Camera camera;
+
 	public float drag = 1;
 	public float minDrag = 0;
 	public float maxDrag = 8;
 	float targetDrag;
+
 	float time;
 	public float minTime = 1;
 	public float maxTime = 2;
-	public Vector3 cameraAngle;
 
 	Quaternion deltaRot;
 	public bool over = false;
@@ -21,7 +21,6 @@ public class ObjectRotation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		cameraAngle = camera.transform.eulerAngles;
 
 		deltaRot = Quaternion.Slerp (deltaRot, Quaternion.identity, Time.deltaTime * drag);
 		transform.rotation = Quaternion.Slerp (transform.rotation, transform.rotation * deltaRot, Time.deltaTime * 5f);
@@ -48,7 +47,7 @@ public class ObjectRotation : MonoBehaviour {
 
 		var lastRot = transform.rotation;
 		var angle_vector = new Vector3 (-Input.GetAxis ("Mouse Y"), Input.GetAxis ("Mouse X"), 0);
-		transform.Rotate (angle_vector * speed *1, Space.World);
+		transform.Rotate (angle_vector * speed *-1, Space.World);
 		deltaRot = Quaternion.Inverse (lastRot) * transform.rotation;
 
 
